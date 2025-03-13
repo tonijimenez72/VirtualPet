539884228833e724271db70db1c7d5c0cfbefd62
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-03-13 11:30:06 +0100

    Initial commit on dev branch

---
+++ src/main/java/cat/itacademy/s05/t02/virtualpet/VirtualpetApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t02.virtualpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetApplication.class, args);
+	}
+
+}
